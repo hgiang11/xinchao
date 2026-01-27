9a0997e6b96a454636448908e671d89b528824e4
hgiang11 <gtran6061@gmail.com>
2026-01-27 10:35:34 +0700

    Add files via upload

---
+++ javaOOP/MaxFileText/src/ReadAndWriteFile.java
@@ -0,0 +1,48 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+import java.io.IOException;
+
+
+
+public class ReadAndWriteFile {
+    public List<Integer> readFile(String filePath){
+        List<Integer> numbers = new ArrayList<>();
+        try {
+            File file = new File(filePath);
+
+            if (!file.exists()) {
+                throw new FileNotFoundException();
+            }
+
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String line = "";
+            while ((line = br.readLine()) != null) {
+                numbers.add(Integer.parseInt(line));
+            }
+            br.close();
+        } catch (Exception e) {
+            System.err.println("Fie không tồn tại or nội dung có lỗi!");
+        }
+        return numbers;
+    }
+
+
+
+
+    public void writeFile(String filePath, int max){
+        try {
+            FileWriter writer = new FileWriter(filePath, true);
+            BufferedWriter bufferedWriter = new BufferedWriter(writer);
+            bufferedWriter.write("Giá trị lớn nhất là: " + max);
+            bufferedWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
